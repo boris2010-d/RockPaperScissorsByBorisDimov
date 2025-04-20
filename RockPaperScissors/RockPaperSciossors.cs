71cb2294723b4bea3ff4b4f683d9e5a947368739
Boris <boris.dimov.highschool@buditel.bg>
2025-04-20 13:23:42 +0300

    - initial commit

---
+++ RockPaperScissors/RockPaperSciossors.cs
@@ -0,0 +1,58 @@
+﻿using System.Runtime.InteropServices;
+
+const string Rock = "Rock";
+const string Paper = "Paper";
+const string Scissors = "Scissors";
+Console.WriteLine("Choose [r]ock [p]aper [s]cissors: ");
+string playerMove = Console.ReadLine();
+if(playerMove == "r" || playerMove == "Rock")
+{
+    playerMove = Rock; 
+}
+if (playerMove == "p" || playerMove == "Paper")
+{
+    playerMove = Rock;
+}
+if (playerMove == "s" || playerMove == "Scissors")
+{
+    playerMove = Rock;
+}
+else
+{
+    Console.WriteLine("Invalid Input . Try Again...");
+}
+Random random = new Random();
+int computerRandomNumber = random.Next(1, 4);
+string computerMove = "";
+
+switch (computerRandomNumber) 
+{
+    case 1:
+        computerMove = Rock;
+        break;
+    case 2:
+        computerMove = Paper;
+        break;
+    case 3:
+        computerMove = Scissors;
+        break;
+}
+Console.WriteLine($"The computer chose {computerMove}.");
+
+if ((playerMove == Rock && computerMove == Scissors) ||
+(playerMove == Paper && computerMove == Rock) ||
+(playerMove == Scissors && computerMove == Paper))
+    {
+    Console.WriteLine("You win.");
+}
+
+else if ((playerMove == Rock && computerMove == Paper) ||
+(playerMove == Paper && computerMove == Scissors) ||
+(playerMove == Scissors && computerMove == Rock))
+{
+    Console.WriteLine("You lose.");
+}
+else
+{
+    Console.WriteLine("This game was a Draw.");
+}
